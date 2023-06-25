b945ffb0436d361489dc5a898f474a9b1799ee42
teddy <teddywambua5456@gmail.com>
2023-06-25 23:39:21 +0300

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,12 @@
-class StudentsController < ApplicationController
 
+class StudentsController < ApplicationController
   def index
     students = Student.all
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :students, only: [:index, :show]
+  
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
